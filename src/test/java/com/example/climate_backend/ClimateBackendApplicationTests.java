7dc8459fd7810fb6bea2c7c8338fe00611356304
yeilkk <yeil99@naver.com>
2025-01-31 20:42:50 +0900

    init: project setting

---
+++ src/test/java/com/example/climate_backend/ClimateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.climate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
