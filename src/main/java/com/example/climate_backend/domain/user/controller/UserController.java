d67f390ccc8d1b51ea16996d2aad62db060443c9
yeilkk <yeil99@naver.com>
2025-02-13 16:30:44 +0900

    feat: user, post API 문서 추가

--- src/main/java/com/example/climate_backend/domain/user/controller/UserController.java
+++ src/main/java/com/example/climate_backend/domain/user/controller/UserController.java
@@ -4,6 +4,8 @@ import com.example.climate_backend.domain.user.dto.request.LoginDto;
 import com.example.climate_backend.domain.user.dto.request.SignupDto;
 import com.example.climate_backend.domain.user.dto.response.UserInfoDto;
 import com.example.climate_backend.domain.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,16 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
+@Tag(name = "User API", description = "회원가입 및 로그인")
 public class UserController {
 
     private final UserService userService;
 
+    @Operation(summary = "회원가입")
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody SignupDto signupDto){
         userService.signup(signupDto);
         return ResponseEntity.ok("회원가입이 완료되었습니다.");
     }
 
+    @Operation(summary = "로그인")
     @PostMapping("/login")
     public ResponseEntity<UserInfoDto> login(@RequestBody LoginDto loginDto){
         return ResponseEntity.ok(userService.login(loginDto));
