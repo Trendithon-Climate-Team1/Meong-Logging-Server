5536452d7d3a5a000d5ea3c592a82ed30820d60b
yeilkk <yeil99@naver.com>
2025-02-19 03:23:21 +0900

    test: datasource 값 테스트

--- src/test/java/com/example/climate_backend/ClimateBackendApplicationTests.java
+++ src/test/java/com/example/climate_backend/ClimateBackendApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.climate_backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ClimateBackendApplicationTests {
 
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+
     @Test
     void contextLoads() {
+        System.out.println("url = " + url);
+        System.out.println("username = " + username);
+        System.out.println("password = " + password);
     }
 
 }
